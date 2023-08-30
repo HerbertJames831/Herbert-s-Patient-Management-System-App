877ab1ec3d944d021657a40c605d47b57e9f7334
HerbertJames831 <herbertjames831@gmail.com>
2023-08-30 01:23:56 +0100

    Connecting Angular App with Update Patient REST API

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -8,13 +8,35 @@ providedIn: 'root'
 })
 export class PatientService {
 
-private baseURL = "http://localhost:8080/api/pms/patients";
+private base_URL = "http://localhost:8080/api/pms/patients";
+private base_URL_1 = "http://localhost:8080/api/pms/patient";
+
 constructor(private httpClient: HttpClient) { }
 
 getPatientsList(): Observable<Patient[]> {
 
-return this.httpClient.get<Patient[]>(`${this.baseURL}`);
+return this.httpClient.get<Patient[]>(`${this.base_URL}`);
+
+}
+
+createPatient(patient: Patient): Observable<Object>{
+return this.httpClient.post(`${this.base_URL_1}`, patient);
 
 
 }
+
+getPatientById(patientId: number): Observable<Patient>{
+
+return this.httpClient.get<Patient>(`${this.base_URL}/${patientId}`);
+
+}
+
+updatePatient(patientId: number, patient: Patient): Observable<Object>{
+
+return this.httpClient.put(`${this.base_URL}/${patientId}`, patient);
+
+}
+
+
+
 }
